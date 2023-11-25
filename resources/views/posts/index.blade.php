b0360639bbb030e814ccd7137f6381404371e9eb
snnoooo <s.2003-haru@docomo.ne.jp>
2023-11-25 18:35:19 +0900

    初ブログ作成08-2

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,12 +1,23 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     
     <head>
         <meta charset="UTF-8">
-        <title>カリキュラム08-2</title>
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        
     </head>
     
     <body>
-        <h1>初ブログ作成</h1>
+        
+        <h1>Blog Name</h1>
+        <div class='posts'>
+            <div class = 'post'>
+                <h2 class = 'title'>Title</h2>
+                <p class = 'body'>This is a sample body.</p>
+            </div>
+           
+        </div>
     </body>
 </html>
