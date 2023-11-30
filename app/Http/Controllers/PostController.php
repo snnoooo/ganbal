a5bc828a650fed2c62c832bd99101ca2c5c6cc96
snnoooo <s.2003-haru@docomo.ne.jp>
2023-11-30 16:59:25 +0900

    08-4課題345

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     }
     
     public function show(Post $post)
@@ -23,7 +24,7 @@ class PostController extends Controller
         return view('posts.create');
     }
     
-    public function store(Request $request, Post $post)
+    public function store(Post $post, PostRequest $request)
     {
         $input = $request['post'];
         $post->fill($input)->save();
