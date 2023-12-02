7087bb2b301db6e1689ad5b4da258dcd81455620
snnoooo <s.2003-haru@docomo.ne.jp>
2023-12-02 18:49:07 +0900

    09-3

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,33 +1,36 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     
-    <head>
-        <meta charset="UTF-8">
-        <meta name = "viewport" content = "width=device-width, initial-scale = 1">
-        <title>Posts</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <x-app-layout>
+        <x-slot name="header">
+            <head>
+                <meta charset="UTF-8">
+                <meta name = "viewport" content = "width=device-width, initial-scale = 1">
+                <title>Posts</title>
+                <!-- Fonts -->
+                <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+            </head>
+        </x-slot>
         
-    </head>
-    
-    <body>
-        
-        <body class = "antialiased">
-            <h1 class = 'title'>
-                {{ $post->title }}
-            </h1>
-            <div class = 'content'>
-                <div class = 'content_post'>
-                    <h3>本文</h3>
-                    <p class = 'body'>{{ $post->body }}</p>
-                    <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+        <body>
+            
+            <body class = "antialiased">
+                <h1 class = 'title'>
+                    {{ $post->title }}
+                </h1>
+                <div class = 'content'>
+                    <div class = 'content_post'>
+                        <h3>本文</h3>
+                        <p class = 'body'>{{ $post->body }}</p>
+                        <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+                    </div>
                 </div>
-            </div>
-            <div class = 'edit'>
-                <a href = "/posts/{{ $post->id }}/edit">edit</a>
-            </div>
-            <div class = 'footer'>
-                <a href = "/">戻る</a>
-            </div>
-        </body>
+                <div class = 'edit'>
+                    <a href = "/posts/{{ $post->id }}/edit">edit</a>
+                </div>
+                <div class = 'footer'>
+                    <a href = "/">戻る</a>
+                </div>
+            </body>
+    </x-app-layout>
 </html>
